a1f7357fca468c5cb694a474833e5a4e2d407098
bam <juhyun19981129@gmail.com>
2021-12-17 18:10:38 +0900

    react-icons 패키지 추가

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, {Fragment} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
-import Home from './codes/router/Home';
-import About from './codes/router/About';
-import User from './codes/router/User';
+import React from 'react';
+import {MdAccessibilityNew} from 'react-icons/md';
 
 const App = () => {
     return (
-        <Fragment>
-            <div>
-                <Link to="/">Home</Link>
-                <br/>
-                <Link to="/about">About</Link>
-                <br/>
-                <Link to="/user/Bam">Bam's info</Link>
-                <br/>
-                <Link to="/user/k">k's info</Link>
-            </div>
-            <div>
-                <Routes>
-                    <Route path="/" element={<Home/>} exact={true}/>
-                    <Route path="/about1" element={<About/>}/>
-                    <Route path="/user/:userName" element={<User/>}/>
-                </Routes>
-            </div>
-        </Fragment>
+        <button>
+            <MdAccessibilityNew />
+        </button>
     );
 };
 
