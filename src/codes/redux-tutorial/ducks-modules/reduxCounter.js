c8cb51c9f0421e7ba201942a9495e56afa36bed3
bam <juhyun19981129@gmail.com>
2022-01-06 14:38:40 +0900

    redux-actions와 immer 불변성

--- src/codes/redux-tutorial/ducks-modules/reduxCounter.js
+++ src/codes/redux-tutorial/ducks-modules/reduxCounter.js
@@ -1,31 +1,21 @@
+import {createAction, handleActions} from 'redux-actions'
+
 const INCREASE = 'reduxCounter/INCREASE';
 const DECREASE = 'reduxCounter/DECREASE';
 
-export const increase = () => ({
-    type: INCREASE,
-});
-
-export const decrease = () => ({
-    type: DECREASE,
-});
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
     number: 0
 };
 
-function reduxCounter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1
-            };
-        case DECREASE:
-            return {
-                number: state.number - 1
-            };
-        default:
-            return state;
-    }
-};
+const reduxCounter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({number: state.number + 1}),
+        [DECREASE]: (state, action) => ({number: state.number - 1}),
+    },
+    initialState,
+);
 
 export default reduxCounter;
